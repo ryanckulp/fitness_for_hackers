f196af651d043806bee7d3e4b3fc14e229cf55c5
Ryan Kulp <ryanckulp@gmail.com>
2020-02-29 15:16:19 -0500

    thumbnails

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,22 @@ class PagesController < ApplicationController
   end
 
   def video_guides
+    @video_guides = [
+      ['Dips', ''],
+      ['Tricep Press Downs', ''],
+      ['Bench Press', ''],
+      ['Pull Downs', ''],
+      ['Chest Cable Fly', ''],
+      ['Rows', ''],
+      ['Deadlifts', ''],
+      ['Bicep Curls', ''],
+      ['Chin-Ups', ''],
+      ['Dumbbell Shrugs', ''],
+      ['Calf Raises', ''],
+      ['Squats', ''],
+      ['Dumbbell Lateral Raise', ''],
+      ['Military Press', ''],
+      ['Leg Extension', '']
+    ]
   end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,5 @@
 module PagesHelper
+  def video_guide_image(workout_name)
+    image_tag "workouts/#{workout_name.downcase.gsub(' ', '-')}.jpg"
+  end
 end
