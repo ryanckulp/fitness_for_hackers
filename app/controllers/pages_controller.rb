edab3783583df2236e44ce38b73bfb7775e7c1ba
Ryan Kulp <ryanckulp@gmail.com>
2020-02-29 17:39:50 -0500

    finish video guides

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,21 +4,22 @@ class PagesController < ApplicationController
 
   def video_guides
     @video_guides = [
-      ['Dips', ''],
-      ['Tricep Press Downs', ''],
-      ['Bench Press', ''],
-      ['Pull Downs', ''],
-      ['Chest Cable Fly', ''],
-      ['Rows', ''],
-      ['Deadlifts', ''],
-      ['Bicep Curls', ''],
-      ['Chin-Ups', ''],
-      ['Dumbbell Shrugs', ''],
-      ['Calf Raises', ''],
-      ['Squats', ''],
-      ['Dumbbell Lateral Raise', ''],
-      ['Military Press', ''],
-      ['Leg Extension', '']
+      ['Dips', 'E3MKxJBlT8U'],
+      ['Tricep Press Downs', 'gkIgZE6r_mw'],
+      ['Bench Press', '4odTSCzAsCE'],
+      ['Pull Downs', 'MEZFFemaI68'],
+      ['Chest Cable Fly', '2pmCb4-m_1g'],
+      ['Rows', 'P2a95uDlBdw'],
+      ['Deadlifts', 'PVpFFrmjykc'],
+      ['Bicep Curls', 'Tj7l7wSIg1g'],
+      ['Chin-Ups', 'ASeQVeORhdU'],
+      ['Pull-Ups', 'uvJEYLaCnPE'],
+      ['Dumbbell Shrugs', 'vNBzoL0Sflk'],
+      ['Calf Raises', '5L8BQSY3JIY'],
+      ['Squats', 'JSXSM2dN41E'],
+      ['Dumbbell Lateral Raise', 'VPbl3kNhuWo'],
+      ['Military Press', 'B6IMJsyLCqQ'],
+      ['Leg Extension', 'xHAbyvj-iP0']
     ]
   end
 end
